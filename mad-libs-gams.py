3aa63505f17e88a4e8f46cadbbc6db17789cf34b
shaharearalamin <shaharearalamin@gmail.com>
2025-06-17 23:16:50 +0600

    add mad libs gams

---
+++ mad-libs-gams.py
@@ -0,0 +1,10 @@
+
+# Mad libs Gams ---> user ar theke input nicci ar amra amader moto kore random ekta code ar moddhe amader variable gulo print kore dicci 
+username= input("Enter is your name:")
+ages= input("How old are you:")
+fathers_name= input("Enter your fathers name:")
+mothers_name= input("Enter your mothers name:")
+study= input("which class do you read in:")
+
+print(f"My cat name is {username}, My cat age is {ages} years old, My cat father name is {fathers_name},"
+      f"My cat mother name is {mothers_name}, My cat study in {study}")
---
+++ main.py
@@ -0,0 +1,2 @@
+with open("mad-libs-gams.py", 'w') as new_file:
+    new_file.write("stor code")
